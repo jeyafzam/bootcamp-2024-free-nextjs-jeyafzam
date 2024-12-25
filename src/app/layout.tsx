860520a8844013adf98c1c96d364a3a4c7d89528
angel <jeyafzamfereshteh87@gmail.com>
2024-12-25 23:25:20 +0330

    [ADD] header and footer component [UPDATE] home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,11 @@
+import { ReactElement } from "react";
+
 import type { Metadata } from "next";
 import { Vazirmatn } from "next/font/google";
+
+import HeaderComponent from "@/components/header/header.component";
+import FooterComponent from "@/components/footer/footer.component";
+
 import "./globals.css";
 
 const vazirmatn = Vazirmatn({
@@ -9,17 +15,24 @@ const vazirmatn = Vazirmatn({
 
 export const metadata: Metadata = {
   title: "Visinit",
-  description: "پلتفرم جامع جستجوی دکتر و رزرو نوبت آنلاین ",
+  description: "پلتفرم جامع جستجوی دکتر و رزرو نوبت آنلاین",
 };
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
-}>) {
+}>): ReactElement {
   return (
     <html lang="fa" dir="rtl" className={vazirmatn.className}>
-      <body>{children}</body>
+      <body>
+        <HeaderComponent />
+        <main>{children}</main>
+        <p className="tagline">
+          نوبت دهی پزشکی، سامانه نوبت دهی اینترنتی بیمارستان و پزشکان
+        </p>
+        <FooterComponent />
+      </body>
     </html>
   );
 }
